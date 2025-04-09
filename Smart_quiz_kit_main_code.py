4ad429df19a1fbc6d385b2dc7f4d155997c00b92
unknown <janruzzellorquia88@gmail.com>
2025-04-09 14:13:06 +0800

    modified line 5 - 14 to ask the user to input a proper amount of items the quiz will have if they input an incorrect amount

--- Smart_quiz_kit_main_code.py
+++ Smart_quiz_kit_main_code.py
@@ -1,10 +1,21 @@
 #function that creates quizzes
 def create_quiz():
     subject = input("Enter the subject of your quiz: ")
-    items_total = int(input("How many items will your quiz have?: "))
-    items_counter = 0
-    quiz_data = [] #list to store questions
     
+    #function so that the user should input a proper number
+    while True:
+        try:
+            items_total = int(input("How many items will your quiz have?: "))
+            if items_total <= 0:
+                print("Please enter a number greater than 0.")
+            else:
+                break
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+        
+        quiz_data = [] #list to store questions
+        
+    #question and answer input        
     while items_counter < items_total:
         question = input("Enter question.\nQuestion "f"{items_counter + 1}. ")
         alphabets = [chr(i) for i in range(97, 101)] # -> letters a-b loop
