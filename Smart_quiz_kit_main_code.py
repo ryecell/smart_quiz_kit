aa63519fa1e59eddd4d0dd4c6237f9918c8e741b
unknown <janruzzellorquia88@gmail.com>
2025-04-09 13:58:57 +0800

    added dictionaries to store quiz data and modified the choice to make a dynamic and cleaner loop using for loop function

--- Smart_quiz_kit_main_code.py
+++ Smart_quiz_kit_main_code.py
@@ -3,15 +3,23 @@ def create_quiz():
     subject = input("Enter the subject of your quiz: ")
     items_total = int(input("How many items will your quiz have?: "))
     items_counter = 0
+    quiz_data = [] #list to store questions
     
     while items_counter < items_total:
-        question = input(f"{items_counter + 1}.")
-        a = input("a. ")
-        b = input("b. ")
-        c = input("c. ")
-        d = input("d. ")
-        correct_answer = input("Correct answer: ")
+        question = input("Enter question.\nQuestion "f"{items_counter + 1}. ")
+        alphabets = [chr(i) for i in range(97, 101)] # -> letters a-b loop
+        choices = {} 
+        for letters in alphabets:
+            choices[letters] = input(f"Choice {letters}: ")
+            
+        correct_answer = input("Correct answer(a-d): ")
         
         items_counter += 1
-
         
+        question_data = {
+            "Question": question,
+            "Choices": choices,
+            "Correct answer": correct_answer
+        }
+        
+create_quiz()
