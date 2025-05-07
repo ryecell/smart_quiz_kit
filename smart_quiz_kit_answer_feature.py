0ca285b8d89405f114196bcf1d63d2b52bdff361
unknown <janruzzellorquia88@gmail.com>
2025-05-07 21:32:08 +0800

    Information varibles

---
+++ smart_quiz_kit_answer_feature.py
@@ -0,0 +1,6 @@
+#enter student info
+student_name = input("Enter your full name (LN, FN MI.): ")
+section = input("Enter your section(ex. BSCPE 1-2): ")
+subject = input("Enter the subject you want to answer: ")
+
+score = 0
