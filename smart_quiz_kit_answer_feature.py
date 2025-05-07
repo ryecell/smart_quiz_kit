708385e8e4a1c8764aa7e6b84da5ccbd7a90e64e
unknown <janruzzellorquia88@gmail.com>
2025-05-07 22:19:14 +0800

    added the file reader function

--- smart_quiz_kit_answer_feature.py
+++ smart_quiz_kit_answer_feature.py
@@ -3,4 +3,15 @@ student_name = input("Enter your full name (LN, FN MI.): ")
 section = input("Enter your section(ex. BSCPE 1-2): ")
 subject = input("Enter the subject you want to answer: ")
 
+file_path = subject +'_quiz.txt' 
+try:
+    file = open(file_path, 'r')
+    print(file)
+    lines = file.readlines()
+    for line in lines:
+        print(line.strip())
+        
+except FileNotFoundError:
+    print(f"Error: Subject does not exist")
+    print(file_path)
 score = 0
