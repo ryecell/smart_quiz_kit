014965a2b7f4a8269c24f8b3dde8ad8b8804420f
unknown <janruzzellorquia88@gmail.com>
2025-05-31 18:03:38 +0800

    modified for proper choice input

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ from utilities.smart_quiz_kit_quiz_creator_in_oop_format import (greeting,quizbu
 user_name = input("Name: ")
 greet = greeting(user_name)
 greet.hello()
-mode = int(input())
+
 
 class option:
-    def __init__(self,choice):
-        self.choice = choice
+    def __init__(self):
+        self.choice = ""
     
     def choices(self):
-        try:
-            if self.choice == 1:
-                quiz = quizbuilder()
-                quiz.start_quiz_creation()
-        except ValueError or int (not (1 and 2)):
-            print("Error! Please enter either 1 or 2")
+        self.choice = int(input())
+        while True:
+            try:
+                if self.choice == 1:
+                    quiz = quizbuilder()
+                    quiz.start_quiz_creation()
+            except ValueError or int (not (1 and 2)):
+                print("Error! Please enter either 1 or 2")
 
-project = option(mode)
+project = option()
 project.choices()
