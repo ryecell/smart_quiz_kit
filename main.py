741faac0613d20a9c5295005b82c2f4dbe4534a6
unknown <janruzzellorquia88@gmail.com>
2025-06-01 16:37:01 +0800

    added the take_quiz function from quiz_taker class

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from utilities.smart_quiz_kit_quiz_creator_in_oop_format import (greeting,quizbuilder)
+from utilities.smart_quiz_kit_quiz_answer_in_oop_format import(quiz_taker)
+
 user_name = input("Name: ")
 greet = greeting(user_name)
 greet.hello()
@@ -13,10 +15,14 @@ class option:
             try:
                 self.choice = int(input())
                 if self.choice == 1:
-                    quiz = quizbuilder()
-                    quiz.start_quiz_creation()
+                    create = quizbuilder()
+                    create.start_quiz_creation()
+                elif self.choice == 2:
+                    answer = quiz_taker()
+                    answer.take_quiz()
                 else:
                     print("The options are only 1 or 2")
+                    
             except ValueError:
                 print("Error! Please enter either 1 or 2")
 
